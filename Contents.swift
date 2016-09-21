02a828d24f1b3cfc3f68216be19f4708be7992ff
themoffat28 <bthemoffat@gmail.com>
2016-09-21 09:35:28 -0600

    Started UI project

---
+++ Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var DynamicView = UIView(frame: CGRectMake(10, 200, 100, 100))
+DynamicView.backgroundColor=UIColor.redColor()
+DynamicView.layer.cornerRadius=30
+DynamicView.layer.borderWidth=2
